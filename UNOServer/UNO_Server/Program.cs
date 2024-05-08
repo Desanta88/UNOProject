4ca63bcb157ad6f5eb752d2a344c2685a1a4fd0d
Desanta88 <101564257+Desanta88@users.noreply.github.com>
2024-05-08 08:24:45 +0200

    UNOSever
    fngnfn

---
+++ UNOServer/UNO_Server/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace UNO_Server
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int porta = 8000;
+            int nClient = 0;
+            string username = "";
+            TcpClient Client = new TcpClient();
+            Server server = new Server(porta);
+            server.Start();
+
+        }
+    }
+}
